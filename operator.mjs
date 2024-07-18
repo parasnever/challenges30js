0187348fbad7cb63e290db8694edb7e244fecbf8
BecomingParas <123073613+parasnever@users.noreply.github.com>
2024-07-18 08:29:39 +0545

    hijoko

---
+++ operator.mjs
@@ -0,0 +1,65 @@
+/*** activity 1 : Arithmetic operations********/ 
+// task one: to add two number 
+
+const num1 = 1
+const num2 = 2
+const sum = num1 + num2
+console.log(sum); //result = 3
+
+// task two : to subtract two number 
+const num_1 = 6
+const num_2 = 3
+const sub = num_1 - num_2
+console.log(sub);// result = 3
+
+
+//task three : to multiply two number
+
+const number1 = 4
+const number2 = 5
+const mul = number1 * number2
+console.log(mul); //result = 20
+
+
+// task four : to divide two number
+
+const number_1 = 6
+const number_2 = 3
+const div = number_1 / number_2
+console.log(div);  // result = 2
+
+
+// task five : to find remainder 
+let  first = 5
+let second = 2
+const rem = first % second
+console.log(rem);
+
+
+/********** Activity 2 : Assignment operator******** */
+
+// task six : += operator used
+
+first += 4
+console.log(first);
+
+// task seven: -= operator used
+
+second -= 1
+console.log(second);
+
+
+/***Activity three: comparison operator */
+
+if (num1 > num2){
+    console.log("num1 is greater :",num1);
+}
+else if(num1 <num2){
+    console.log("num2 is greter : ");
+}
+
+
+
+
+
+
